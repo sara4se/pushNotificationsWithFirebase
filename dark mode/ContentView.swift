f1eb96d0aa01c8b82826e7b7e2dff0404c304ae6
sara <sara.ahumidi4@gmail.com>
2022-12-06 10:04:54 +0300

    Initial Commit

---
+++ dark mode/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  dark mode
+//
+//  Created by Sara Alhumidi on 12/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dark mode/dark_modeApp.swift
@@ -0,0 +1,17 @@
+//
+//  dark_modeApp.swift
+//  dark mode
+//
+//  Created by Sara Alhumidi on 12/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct dark_modeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
